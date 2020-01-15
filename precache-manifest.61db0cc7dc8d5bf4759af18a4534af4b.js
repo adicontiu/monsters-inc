155e21d9136a118c7b50437053b8da4ac56bb85f
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-15 17:31:55 +0200

    Updates

---
+++ precache-manifest.61db0cc7dc8d5bf4759af18a4534af4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d420ddc6ee0a7d01ab476f355a91c9ed",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "5983998090af6365963a",
+    "url": "/monsters-inc/static/css/main.96941aee.chunk.css"
+  },
+  {
+    "revision": "4b687edbf98f548ea48a",
+    "url": "/monsters-inc/static/js/2.0d70f0f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-inc/static/js/2.0d70f0f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5983998090af6365963a",
+    "url": "/monsters-inc/static/js/main.a4a47433.chunk.js"
+  },
+  {
+    "revision": "ea5c4581378594cf44ae",
+    "url": "/monsters-inc/static/js/runtime-main.88751db6.js"
+  }
+]);
